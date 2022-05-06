9de6fdf51c543a6067198b4582e147e4c6ceb12c
Esbol Moldrahmetov <esbolarai@gmail.com>
2022-05-06 12:29:12 +0600

    feat(layout): add default (QUWI-2)

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="default-layout">
+    <TopNavbar class="top-navbar" />
+    <Nuxt />
+  </main>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
+<style scoped>
+.default-layout {
+  display: grid;
+  height: 100vh;
+  grid-template-rows: auto 1fr;
+}
+</style>
