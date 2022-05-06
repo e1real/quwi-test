d4af1475245969bd472f8af82ab784ee9b5b8823
Esbol Moldrahmetov <esbolarai@gmail.com>
2022-05-06 12:25:41 +0600

    feat(components): add topnavbar (QUWI-2)

---
+++ components/TopNavbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="nav">
+    <ul class="nav__links nav-link-list">
+      <nuxt-link class="nav-link-list__item" tag="li" to="/">Home</nuxt-link>
+      <nuxt-link class="nav-link-list__item" tag="li" to="/about">About</nuxt-link>
+    </ul>
+    <div class="auth-buttons">
+      <nuxt-link class="login-button" to="/auth/login">Login</nuxt-link>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: 'TopNavbar',
+}
+</script>
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  padding: 24px 48px;
+  background-color: #5d5d5a;
+
+  &__links {
+    margin-right: auto;
+  }
+}
+
+.nav-link-list {
+  display: flex;
+  flex-wrap: wrap;
+
+  &__item {
+    color: #ffcdab;
+
+    &:not(:last-child) {
+      margin-right: 24px;
+    }
+  }
+}
+
+.login-button {
+  color: #fff4e3;
+}
+</style>
