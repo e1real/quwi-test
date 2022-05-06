2bec7480df35f85372ff6e09972357037aac6352
Esbol Moldrahmetov <esbolarai@gmail.com>
2022-05-06 14:53:51 +0600

    feat(pages): add proect list page (QUWI-3)

---
+++ components/ProjectCard.vue
@@ -0,0 +1,97 @@
+<template>
+  <component :is="tag" class="project-card">
+    <img class="project-card__logo" :src="project.logo_url" />
+    <div class="project-card__name">{{ project.name }}</div>
+    <div
+      :class="[
+        'project-card-status',
+        'project-card__status',
+        { active: !!project.is_active },
+        { disabled: !project.is_active },
+      ]"
+    >
+      {{ project.is_active ? 'Active' : 'Not active' }}
+    </div>
+    <div class="project-card__time project-card-time">
+      <div class="project-card-time__row">
+        <span class="project-card-time__title">time this week</span>
+        <span class="project-card-time__value">00:00:00</span>
+      </div>
+
+      <div class="project-card-time__row">
+        <h5 class="project-card-time__title">this month</h5>
+        <span class="project-card-time__value">00:00:00</span>
+      </div>
+
+      <div class="project-card-time__row">
+        <h5 class="project-card-time__title">total</h5>
+        <span class="project-card-time__value">00:00:00</span>
+      </div>
+    </div>
+  </component>
+</template>
+<script>
+export default {
+  name: 'ProjectCard',
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    project: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.project-card {
+  display: flex;
+  align-items: center;
+  border: 1px solid whitesmoke;
+  padding: 16px 32px;
+
+  &__logo {
+    width: 70px;
+  }
+
+  &__name {
+    font-weight: bold;
+    margin: 0 18px;
+  }
+
+  &__status {
+    font-weight: bold;
+    margin: 0 18px;
+  }
+}
+
+.project-card-time {
+  display: flex;
+  flex-direction: column;
+
+  &__row {
+    display: flex;
+    padding: 4px;
+  }
+
+  &__title {
+    margin-right: 18px;
+  }
+
+  &__value {
+    font-weight: bold;
+  }
+}
+
+.project-card-status {
+  &.active {
+    color: green;
+  }
+
+  &.disabled {
+    color: red;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>this is index page</div>
+  <div class="project-list-block">
+    <ul class="project-list">
+      <ProjectCard
+        v-for="project in projectList"
+        :key="project.id"
+        class="project-list__item"
+        :project="project"
+        tag="li"
+      />
+    </ul>
+    <div v-if="projectListPending">Pending...</div>
+  </div>
 </template>
 <script>
 export default {
   name: 'HomePage',
   middleware: 'user',
+  data() {
+    return {
+      projectList: [],
+      projectListPending: false,
+    }
+  },
+  mounted() {
+    this.fetchProjectList()
+  },
+  methods: {
+    async fetchProjectList() {
+      try {
+        this.projectListPending = true
+        const { data } = await this.$axios.get(
+          'projects-manage/index?filters[is_active]=1&sort=dta_create'
+        )
+        this.projectList = data.projects
+      } catch (err) {
+        window.console.error(err)
+      } finally {
+        this.projectListPending = false
+      }
+    },
+  },
 }
 </script>
+<style lang="scss" scoped>
+.project-list-block {
+  padding: 40px 0;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
