c69c45dfe61ad8cd580cfdd5ca74f27cad678b0a
Esbol Moldrahmetov <esbolarai@gmail.com>
2022-05-06 12:23:06 +0600

    feat(project): minimum requirement (QUWI-1)

---
+++ plugins/vee-validate.js
@@ -0,0 +1,7 @@
+import { extend } from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+
+Object.keys(rules).forEach(rule => {
+  // eslint-disable-next-line import/namespace
+  extend(rule, rules[rule]);
+});
